3823aaba76b07b7d947d802b04baa1c9f50593f4
NA-star <58888871+NA-star@users.noreply.github.com>
2022-08-26 23:45:23 +0530

    Add files via upload

---
+++ onlinequiz/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| JavaScript Quiz Game |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/book.png) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> JavaScript Quiz Game </h1>
+                <a href="admin.php" class="btn"> Admin Login </a> &emsp;
+	 <a href="login.php" class="btn"> Login </a> &emsp;
+                <a href="register.php" class="btn"> Register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+    </body>
+</html>
